5433bd2aff394c53253f1eb04d6c18330b2bf9ba
Ejrosa9028 <132318096+Ejrosa9028@users.noreply.github.com>
2025-05-11 23:08:40 -0400

    fix

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,12 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrementamos n para evitar un bucle infinito
-    return result
+        n -= 1  # Decrementar n en cada iteración
+        return result
 
-# Verificamos que el argumento haya sido pasado
-if len(sys.argv) > 1:
-    f = factorial(int(sys.argv[1]))  # Convertimos el argumento a entero
-    print(f)
-else:
-    print("Por favor, ingresa un número como argumento.")
+f = factorial(int(sys.argv[1]))
+print(f)
--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import sys
 
-# Empezamos el bucle desde 1 para evitar imprimir el nombre del script
-for i in range(1, len(sys.argv)):
+for i in range(len(sys.argv)):
     print(sys.argv[i])
